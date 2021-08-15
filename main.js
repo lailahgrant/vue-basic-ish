1d8c1d7054c782473d52d542cc2141ca00c08b70
lailahgrant <lailahgrant19@gmail.com>
2021-08-15 11:37:42 +0300

    components in Vuejs

--- main.js
+++ main.js
@@ -1,9 +1,76 @@
-// var product = "Socks"
+Vue.component('product', {
 
-var app = new Vue({
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+    }  
+    },
 
-    el: '#app',
-    data: {
+    template: `
+        
+
+        <div class="product">
+            <div class="product-image">
+                <img v-bind:src="image" alt="" >
+            </div>
+        
+
+            <div class="product-info">
+                <!-- <h1> {{ product }} </h1> -->
+                <h1> {{ title }}  </h1>  <!--- computed property-->
+                <p>{{ description }}</p>
+                <a :href="href">More about Socks</a>
+                <p v-if="inStock">In Stock</p>
+                <p :class="{outOfStock : !inStock }" v-else >Out of Stock</p>
+                <p>{{ onsale }}</p>
+                <!--<p>User is  premium : {{premium}}</p> -->
+                <p>Shipping : {{shipping}}</p>
+
+                <ul>
+                    <span>Material</span>
+                    <li v-for="detail in details" > {{ detail }}</li><br>
+                    <span>Sizes</span>
+                    <li v-for="size in sizes"> {{ size }}</li>
+                </ul>
+
+                <!-- Style Binding - style is bound which targets a css style, :style 
+                it can be "{backgroundColor}" or "{'background-color'}"-->
+                <!-- <div class="color-box" v-for="(variant, index) in variants" :key="variant.variantId" :style="{backgroundColor : variant.variantColor}" @mouseover="updateProduct(variant.variantImage)"> add index in our v-for, instead of passing the variant.variantImage - we pass index  -->
+                    <div class="color-box" v-for="(variant, index) in variants" :key="variant.variantId" :style="{backgroundColor : variant.variantColor}" @mouseover="updateProduct(index)"></div>
+                    <!-- <p @mouseover="updateProduct(variant.variantImage)" >{{ variant.variantColor }}</p> -->
+                
+                
+                                
+                <!-- <button v-on:click="cart +=1 ">Add to Cart</button> -->
+                <!-- :disabled="!inStock" when inStock is false, this attribute binding will disable a button. 
+                We shouldn't click the Add to Cart button when there's no product in stock
+                If stock is empty, the Add to Cart button changes color from clickable to disable - Class binding
+                Class Binding - class name is bound which targets a class name in css, :class-->
+                <button v-on:click="addToCart" :disabled="!inStock" :class="{disabledButton : !inStock}" >Add to Cart</button>
+                <button @click="removeFromCart" :disabled= "!inStock" :class="{disabledButton : !inStock}">-</button>
+
+                <div class="cart">
+                    <p>Cart {{ cart }}</p>
+                </div>
+
+
+
+                <!-- <p v-if="inventory > 10">In Stock</p>
+                <p v-else-if="inventory < 10 && inventory > 0" >Almost out of Stock</p>
+                <p v-else >Out of Stock</p> -->
+
+                <!-- <span v-show="onSale">On Sale</span> -->
+
+            </div>
+        
+        </div>
+    
+    `,
+
+    //make data object into a function that returns data objects
+    data() {
+        return {
         product: "Socks",
         description: "Socks keep our feet warm, soft and feeling comfy",
         // image: "./vmSocks-green.jpg", - replacing image with selectedVariant
@@ -28,6 +95,8 @@ var app = new Vue({
         ],
 
         cart: 0,
+
+        }
     },
 
     //methods property
@@ -73,8 +142,31 @@ var app = new Vue({
                 return this.brand + ' ' + this.product + ' are on sale!'
             }
                 return this.brand + ' ' + this.product + ' are not on sale'
+        },
+
+        //shipping free or 2.99 for premium user
+        shipping() {
+            if (this.premium) {
+                return "Free"
+            }
+            return "2.99"
+                
         }
     }
+
+})
+
+
+// var product = "Socks"
+
+var app = new Vue({
+
+    el: '#app',
+
+    data: {
+        premium:true
+    }
+    
 
 })
 
